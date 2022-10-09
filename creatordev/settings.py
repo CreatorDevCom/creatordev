7f087d8d214398222e0f063977ff51b77b456c17
Creaort Dev <112323941+CreatorDevCom@users.noreply.github.com>
2022-10-09 04:14:23 -0700

    Update settings.py

--- creatordev/settings.py
+++ creatordev/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
  
 # For Deployment
 # STATICFILES_DIRS = os.path.join(BASE_DIR, '/static'), 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static/') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static/') 
  
 
 # heroku setup STATICFILEs
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR , 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR , 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR , 'static'),)
 # django_heroku.settings(locals())
 
 
