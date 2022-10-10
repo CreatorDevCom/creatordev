dd49e90663f1d1b7222973f2353b0663466ce5fd
codeDev911 <winzhi00@gmail.com>
2022-10-10 05:46:16 -0700

    fixed

--- creatordev/urls.py
+++ creatordev/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include  
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static 
+from django.urls import re_path as url
 from api.views import privacyPolices
+from django.views.static import serve
 urlpatterns = [
     path("account/", include("allauth.urls")),
     path('admin/', admin.site.urls),  
@@ -11,7 +13,11 @@ urlpatterns = [
     path("article/",include("article.urls")), 
     path("forum/",include("forum.urls")), 
     path("user/",include("user.urls")),  
-]
+]  
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+url(r'^media/(?P<path>.*)$',serve,{"document_root":settings.MEDIA_ROOT})
+url(r'^static/(?P<path>.*)$',serve,{"document_root":settings.STATIC_ROOT})
