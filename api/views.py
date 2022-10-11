596c335b5d41e446f6fee2a200943163e7ea6d62
codeDev911 <winzhi00@gmail.com>
2022-10-10 21:13:43 -0700

    email son

--- api/views.py
+++ api/views.py
@@ -5,16 +5,18 @@ from django.shortcuts import redirect, render
 
 
 def sendNewAccountCongrates_Mail(request):
-  send_congrates_new_account_mail("Afnan ahad" , "mr13ha24ck57er68@gmail.com")
-  print("Email sended")
-  return response.Response("Email sended")
+  print("Email sended") 
 
 
 
 # Privacy polices
 def privacyPolices(request):
   privacy_polices = PrivacyPolices.objects.all()
-
+  # try:
+  #   send_congrates_new_account_mail("Afnan ahad" , "mr13ha24ck57er68@gmail.com")
+  #   print("email is send")
+  # except :
+  #    print("Not send")
   context = {"privacy_polices":privacy_polices}
 
   return render(request,'privacy_polices.html' ,context )
